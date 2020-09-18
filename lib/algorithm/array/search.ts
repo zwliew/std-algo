21992b6e72916150ad969a2dfc53ccb71967e763
Zhao Wei Liew <zhaoweiliew@gmail.com>
2020-09-18 14:19:34 +0800

    algo/array: Refactor binary search algo

--- lib/algorithm/array/search.ts
+++ lib/algorithm/array/search.ts
@@ -1,53 +1,51 @@
 /**
  * Returns the index of the first element in the array that satisfies the comparator function.
  * The array must be sorted prior to making this call.
- * @param {any} arr The array to be searched
- * @param {any} val The value to search for
- * @param {(lhs: any, rhs: any) => boolean} comp The comparator function to use.
+ * @param {T} arr The array to be searched
+ * @param {T} val The value to search for
+ * @param {(lhs: T, rhs: T) => boolean} comp The comparator function to use.
  * @returns {number} The index of the first element not less than the specified value.
  *                    If no such element exists, the length of the array is returned.
  */
-export function binarySearch(
-  arr: any[],
-  val: any,
-  comp: (lhs: any, rhs: any) => boolean
+export function binarySearch<T>(
+  arr: T[],
+  val: T,
+  comp: (lhs: T, rhs: T) => boolean
 ): number {
-  let lo = 0;
-  let hi = arr.length - 1;
-  let idx = arr.length;
-  while (lo <= hi) {
+  let lo = -1;
+  let hi = arr.length;
+  while (lo < hi - 1) {
     const mid = lo + Math.floor((hi - lo) / 2);
     if (comp(arr[mid], val)) {
-      idx = mid;
-      hi = mid - 1;
+      hi = mid;
     } else {
-      lo = mid + 1;
+      lo = mid;
     }
   }
-  return idx;
+  return hi;
 }
 
 /**
  * Returns the index of the first element in the array that is not less than
  * the specified value. The array must be sorted prior to making this call.
- * @param {any} arr The array to be searched
- * @param {any} val The value to search for
+ * @param {T} arr The array to be searched
+ * @param {T} val The value to search for
  * @returns {number} The index of the first element not less than the specified value.
  *                    If no such element exists, the length of the array is returned.
  */
-export function lowerBound(arr: any[], val: any): number {
+export function lowerBound<T>(arr: T[], val: T): number {
   return binarySearch(arr, val, (lhs, rhs) => lhs >= rhs);
 }
 
 /**
  * Returns the index of the first element in the array that is greater than
  * the specified value. The array must be sorted prior to making this call.
- * @param {any} arr The array to be searched
- * @param {any} val The value to search for
+ * @param {T} arr The array to be searched
+ * @param {T} val The value to search for
  * @returns {number} The index of the first element greater than the specified value.
  *                    If no such element exists, the length of the array is returned.
  */
-export function upperBound(arr: any[], val: any): number {
+export function upperBound<T>(arr: T[], val: T): number {
   return binarySearch(arr, val, (lhs, rhs) => lhs > rhs);
 }
 
@@ -61,6 +59,6 @@ export function upperBound(arr: any[], val: any): number {
  * @returns {[number, number]} The range containing all the elements in the array
  * that are equal to the specified value.
  */
-export function equalRange(arr: any[], val: any): [number, number] {
+export function equalRange<T>(arr: T[], val: T): [number, number] {
   return [lowerBound(arr, val), upperBound(arr, val)];
 }
