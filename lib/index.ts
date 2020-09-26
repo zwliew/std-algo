b47338aed29cb760cd4528cd2bbcfecb6bda7899
Zhao Wei Liew <zhaoweiliew@gmail.com>
2020-09-26 19:30:01 +0800

    Export priority queue

--- lib/index.ts
+++ lib/index.ts
@@ -3,5 +3,6 @@ export * from "./queue";
 export * from "./deque";
 export * from "./linkedList";
 export * from "./monotonicQueue";
+export * from "./priorityQueue";
 export * from "./algorithm";
 export * from "./math";
